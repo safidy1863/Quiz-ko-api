1505114904a5f50ab831b4e7c6e19feba32d212d
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2024-08-15 05:12:57 +0300

    init : user resource
    Signed-off-by: ROBUSTE Manohisafidy <robustemmanuel@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ExampleModule } from './example/example.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ExampleModule],
+  imports: [ExampleModule, UserModule],
 })
 export class AppModule {}
