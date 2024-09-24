ccf10908ff4698accce9aa7671cc0a0c0a0f3aa7
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2024-09-24 17:27:02 +0300

    fix:some warning

--- src/shared/dtos/users/create-user.dto.ts
+++ src/shared/dtos/users/create-user.dto.ts
@@ -1,32 +1,29 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { UserRole } from "@/shared/enums";
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@/shared/enums';
 
 export class CreateUserDto {
-    @ApiProperty({
-        example : 'John'
-    })
-    lastName: string
+  @ApiProperty({
+    example: 'John',
+  })
+  lastName: string;
 
+  @ApiProperty({
+    example: 'Doe',
+  })
+  firstName?: string;
 
-    @ApiProperty({
-        example : 'Doe'
-    })
-    firstName?: string
+  @ApiProperty({
+    example: 'johndoe@gmail.com',
+  })
+  email: string;
 
+  @ApiProperty({
+    example: '123456789',
+  })
+  password: string;
 
-    @ApiProperty({
-        example : 'johndoe@gmail.com'
-    })
-    email : string
-
-
-    @ApiProperty({
-        example : '123456789'
-    })
-    password: string
-
-    @ApiProperty({
-        example : 'STUDENT'
-    })
-    role:  UserRole
+  @ApiProperty({
+    example: 'STUDENT',
+  })
+  role: UserRole;
 }
