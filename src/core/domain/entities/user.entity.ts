af45b031196815c2ae06f807fd97b7d2bfdb0498
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2024-10-07 06:04:00 +0300

    config validation
    Signed-off-by: ROBUSTE Manohisafidy <robustemmanuel@gmail.com>

--- src/core/domain/entities/user.entity.ts
+++ src/core/domain/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity } from "@/core/base"
 import { UserRole } from "@/shared"
+import { IsEmail, IsNotEmpty } from "class-validator"
 
 export class UserEntity extends Entity {
     lastName: string
--- src/shared/dtos/users/create-user.dto.ts
+++ src/shared/dtos/users/create-user.dto.ts
@@ -1,27 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@/shared/enums';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   @ApiProperty({
     example: 'John',
   })
   lastName: string;
+
 
   @ApiProperty({
     example: 'Doe',
   })
   firstName?: string;
 
+
+  @IsNotEmpty()
+  @IsEmail()
   @ApiProperty({
     example: 'johndoe@gmail.com',
   })
   email: string;
 
+  @IsNotEmpty()
   @ApiProperty({
     example: '123456789',
   })
   password: string;
 
+  
   @ApiProperty({
     example: 'STUDENT',
   })
